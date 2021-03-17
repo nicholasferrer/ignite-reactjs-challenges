d8f56256a6cc7277660715376fe5a612971748c5
Nicholas Ferrer <nicholasferrer@hotmail.com>
2021-03-16 21:11:50 -0300

    🎉  Complementary challenge finalized

---
+++ chapter-01/complementary-challenge/src/components/Content.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from '../components/MovieCard';
+import { GenreResponseProps } from './SideBar';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface IContentProps {
+  selectedGenreId: number;
+}
+
+export function Content({ selectedGenreId } : IContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setSelectedGenre(response.data);
+    });
+  }, [selectedGenreId]);
+  
+  return(
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
+}
